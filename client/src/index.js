50dc6f29ddb36be2c4c202d224cbdd37a0894c75
s-rupert <140841484+s-rupert@users.noreply.github.com>
2025-02-25 23:55:28 +0000

    feat: normal changes

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,19 @@ import { Homepage } from './Home';
 import { Project } from './Project'
 import { Achievement } from './Achievement';
 function App() {
+  useEffect(() => {
+    let resizeTimer;
+    const handleResize = () => {
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(() => {
+        window.location.reload();
+      }, 100);
+    };
+
+    window.addEventListener("resize", handleResize);
+    
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <div id="main-section">
       <Homepage />
